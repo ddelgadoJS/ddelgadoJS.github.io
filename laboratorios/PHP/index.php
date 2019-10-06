002b93bc0e523736fd39b461164a502f243e3ae6
Daniel Delgado <delgado.jdds@gmail.com>
2019-10-06 17:44:40 -0600

    Login page working

--- laboratorios/PHP/index.php
+++ laboratorios/PHP/index.php
@@ -1,9 +1,8 @@
 <?php
-    $userName = "Daniel";
-    $password = "1234";
-
     session_start();
     if($_POST == $_SESSION['oldPOST']) $_POST = array(); else $_SESSION['oldPOST'] = $_POST;
+
+    include 'imaginaryDatabase.php';
 
     function showErrorMessage($errorMessageType) {
         if ($errorMessageType == "user") {
@@ -16,17 +15,31 @@
                 <button type='button'' class='close' data-dismiss='alert' aria-hidden='true'>×</button>
                 <span><strong>Error: </strong> Contraseña incorrecta.</span>
                 </div>";
+        } elseif ($errorMessageType == "good") {
+            echo "<div class='alert alert-danger'>
+                <button type='button'' class='close' data-dismiss='alert' aria-hidden='true'>×</button>
+                <span><strong>Error: </strong> GOOOOOOOOD.</span>
+                </div>";
         }
     }
-
+    
     if (isset($_POST['user']) && isset($_POST['password'])) {
-        if ($userName != $_POST['user']) {
-            showErrorMessage("user");
-        } elseif ($password != $_POST['password']) {
-            showErrorMessage("password");
+        foreach ($usersArray as $key => $value) {
+            if ($value->checkUsername($_POST['user'])) { // Check username in DB
+                if ($value->checkPassword($_POST['password'])) { // Check password in DB
+                    // Username and password correct.
+                    echo "<script type='text/javascript'>location.href = '/scripts/mainPage.html'</script>";
+                    break;
+                } else { // Incorrect password
+                    showErrorMessage("password");
+                    break;
+                }
+            }
+
+            if (end($usersArray) == $value) { // If end of array, it means it's not in DB.
+                showErrorMessage("user"); // If username not found, incorrect username.
+            }
         }
-        
-        echo "<script type='text/javascript'>location.href = '/scripts/index.html'</script>";
     }
 
     include 'login.html';
